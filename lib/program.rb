2d280095e9084847b5e60e473cdf28007084a75b
GabiOC <oconnor.gabriele@gmail.com>
2015-08-18 18:32:46 -0400

    Add keyword args to initialize

--- lib/program.rb
+++ lib/program.rb
@@ -35,5 +35,5 @@ class AsciiGif
   end
 end
 
-# attributes = {url: 'http://media.giphy.com/media/26BkN2Uh9wImWDXLa/giphy.gif', width: 100, color: true, loops: 4}
-# AsciiGif.new(attributes).write_frames
+attributes = {url: 'http://media.giphy.com/media/26BkN2Uh9wImWDXLa/giphy.gif', width: 100, color: true, loops: 4}
+AsciiGif.new(attributes).write_frames
